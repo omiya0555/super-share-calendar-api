c54d4ec629420bc9d7312c1a26587b17dd5d5e87
omiya0555 <otoharadak@gmail.com>
2024-11-04 12:10:19 +0900

    Add channels to route/channels.php. And install the pusher.

--- routes/channels.php
+++ routes/channels.php
@@ -2,17 +2,22 @@
 
 use Illuminate\Support\Facades\Broadcast;
 
-/*
-|--------------------------------------------------------------------------
-| Broadcast Channels
-|--------------------------------------------------------------------------
-|
-| Here you may register all of the event broadcasting channels that your
-| application supports. The given channel authorization callbacks are
-| used to check if an authenticated user can listen to the channel.
-|
-*/
+// 個人チャットチャネル
+Broadcast::channel('chat-room.{chatRoomId}', function ($user, $chatRoomId) {
+    return $user->chatRooms()->where('chat_room_id', $chatRoomId)->exists();
+});
 
+// グループチャットチャネル
+Broadcast::channel('group-chat.{chatRoomId}', function ($user, $chatRoomId) {
+    return $user->chatRooms()->where('chat_room_id', $chatRoomId)->where('is_group', true)->exists();
+});
+
+// ユーザー個別通知チャネル
 Broadcast::channel('user.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
+});
+
+// パブリックチャネル
+Broadcast::channel('public-channel', function () {
+    return true;
 });
